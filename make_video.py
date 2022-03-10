c7b8a79ffb0ee797b41075801d75ea5f4f5c29d8
hardikajmani1 <hardikajmani@uchicago.edu>
2022-03-10 15:34:09 -0600

    updates

--- make_video.py
+++ make_video.py
@@ -2,18 +2,20 @@
 import cv2
 import os
 
-image_folder = '../video1'
-video_name = 'video.mp4'
-fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+folders = ['../video1', '../video2']
+for i in range(len(folders)):
+    image_folder = folders[i]
+    video_name = os.path.join(image_folder, 'video{}.mp4'.format(i))
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
 
-images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
-frame = cv2.imread(os.path.join(image_folder, images[0]))
-height, width, layers = frame.shape
+    images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+    frame = cv2.imread(os.path.join(image_folder, images[0]))
+    height, width, layers = frame.shape
 
-video = cv2.VideoWriter(video_name, fourcc, 12.0, (width,height))
+    video = cv2.VideoWriter(video_name, fourcc, 12.0, (width,height))
 
-for image in images:
-    video.write(cv2.imread(os.path.join(image_folder, image)))
+    for image in images:
+        video.write(cv2.imread(os.path.join(image_folder, image)))
 
-cv2.destroyAllWindows()
-video.release()
+    cv2.destroyAllWindows()
+    video.release()
